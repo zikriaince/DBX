e2ff59d0919cfa863c07477f2d3572fc16918b7b
zikriaince <148481901+zikriaince@users.noreply.github.com>
2025-09-11 11:07:16 -0700

    Update pull_new_data.sh

--- pull_new_data.sh
+++ pull_new_data.sh
@@ -1,4 +1,6 @@
 https://customerconnect.vmware.com/en/downloads/details?downloadGroup=CART23FQ4_MAC_2212.1&productId=1027&rPId=107193
+https://customerconnect.omnissa.com/downloads/details?downloadGroup=CART26FQ2_MAC_2506&productId=1616&rPId=119131
+https://customerconnect.omnissa.com/en/downloads/details?downloadGroup=CART23FQ4_MAC_2212.1&productId=1027&rPId=107193
 
 
 #!/bin/bash
